cba33b9b112a6c5a73f240e876ee8e82f091899c
Lucho <luchoster@gmail.com>
2019-03-10 23:53:20 -0700

    Fixing a var name that had a  instead of . Removed the zip file and the locations page, since this is not necessary

--- routes/index.php
+++ routes/index.php
@@ -6,6 +6,6 @@
   /* require_once 'items_by_cat.php'; */
 
   add_action('rest_api_init', 'registerPageRoutes');
+  add_action('rest_api_init', 'registerPostRoutes');
   /* add_action('rest_api_init', 'registerCategoryRoutes'); */
   /* add_action('rest_api_init', 'registerItemsRoutes'); */
-  add_action('rest_api_init', 'registerPostRoutes');
